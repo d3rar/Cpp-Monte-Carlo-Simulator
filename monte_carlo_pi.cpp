54bf81eda4226a7a11d36fe9d0b20cb8f2a71740
d3rar <d3rar@outlook.com>
2025-07-13 10:03:49 +0200

    Final

--- monte_carlo_pi.cpp
+++ monte_carlo_pi.cpp
@@ -1,18 +1,28 @@
-#include <iostream> // input and output (std::cout)
-#include <cstdlib> // rand(), srand(), and RAND_MAX
-#include <ctime>  // for time() 
-#include <iomanip> // std::fixed and std::setprecision
+#include <iostream>  // for input/output
+#include <cstdlib>  // for rand(), srand(), RAND_MAX
+#include <ctime>   // for time()
+#include <iomanip>  // for setprecision and fixed
 
 int main() {
-    std::srand(std::time(0));  // initialize a random number generator with current sys time
+    std::srand(std::time(0));  // seed random number generator
 
-    for (int i = 0; i < 20; ++i) {  // iterate 20 times
-        
-        double x = static_cast<double>(rand()) / RAND_MAX; //random x coordinate between 0.0 and 1.0
-        double y = static_cast<double>(rand()) / RAND_MAX; //random y coordinate between 0.0 and 1.0
-        // print r with only 2 decimals x, y coordinates 
-        std::cout << std::fixed << std::setprecision(2) << "(" << x <<", " << y << ")" << std::endl; 
+    int totalPoints = 0;
+    int pointsInsideCircle = 0;
+
+    for (int i = 0; i < 10000; ++i) {  
+        double x = static_cast<double>(rand()) / RAND_MAX;
+        double y = static_cast<double>(rand()) / RAND_MAX;
+
+        totalPoints++;
+
+        if (x * x + y * y <= 1) {
+            pointsInsideCircle++;
+        }
     }
+    double piEstimate = 4.0 * pointsInsideCircle / totalPoints;
 
-    return 0; 
+    std::cout << std::fixed << std::setprecision(6);
+    std::cout << "Estimated pi: " << piEstimate << std::endl;
+
+    return 0;
 }
